17b809a40da5e5926296fb0fb1390268d63f4a69
wiffel <wiffel@tilient.net>
2017-12-18 19:50:42 +0100

    v0.2

--- src/main/kotlin/org/tilient/learn/App.kt
+++ src/main/kotlin/org/tilient/learn/App.kt
@@ -3,4 +3,11 @@ package org.tilient.learn
 fun main(args: Array<String>)
 {
   println("Learning Kotlin ...")
+  println("-------------------")
+  val arr = arrayOf(1,2,3,4)
+  for (nr in arr) { println(nr) }
+  println("-------------------")
+  val nrs = (2..20).step(2).reversed()
+  for (nr in nrs) { println(nr) }
+  println("-------------------")
 }
